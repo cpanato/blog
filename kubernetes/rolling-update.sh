48a0c489b3b2ed378f4d8f3bf8840062fb7fba38
cpanato <ctadeu@gmail.com>
2018-05-11 15:13:42 +0200

    4

--- kubernetes/rolling-update.sh
+++ kubernetes/rolling-update.sh
@@ -2,18 +2,21 @@
 
 export BLOG_TAG="1.3"
 
-docker pull ${DOCKER_USER}/blog:BLOG_TAG
-if [ $? -eq 0 ]
-then
-   echo "Image already exist. Aborting build and deploy"
-   echo "Please bump the version to create a new image"
-   exit 1
+function docker_tag_exists() {
+    curl --silent -f -lSL https://index.docker.io/v1/repositories/$1/tags/$2 > /dev/null
+}
+
+if docker_tag_exists ${DOCKER_USER}/blog BLOG_TAG; then
+  echo "Image already exist. Aborting build and deploy"
+  echo "Please bump the version to create a new image"
+  exit 1
+else
+  echo "Start to build new docker image"
+  docker build -t ${DOCKER_USER}/blog:${BLOG_TAG} --rm docker
+  docker push ${DOCKER_USER}/blog:${BLOG_TAG}
+  docker rmi $(docker images --filter=reference="${DOCKER_USER}/blog" -q)
+
+  echo "Start to deploy in k8s"
+  kubectl set image deployment/blog blog="$DOCKER_USER"/blog:$BLOG_TAG
 fi
 
-echo "Start to build new docker image"
-docker build -t ${DOCKER_USER}/blog:${BLOG_TAG} --rm docker
-docker push ${DOCKER_USER}/blog:${BLOG_TAG}
-docker rmi $(docker images --filter=reference="${DOCKER_USER}/blog" -q)
-
-echo "Start to deploy in k8s"
-kubectl set image deployment/blog blog="$DOCKER_USER"/blog:$BLOG_TAG
